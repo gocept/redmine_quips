750e332d1d48e36dc2d52ab46f396d564a9ed339
Wolfgang Schnerring <wosc@wosc.de>
2009-03-12 16:15:46 +0000

    implemented retrieving a random quip

--- app/models/quip.rb
+++ app/models/quip.rb
@@ -1,7 +1,11 @@
 class Quip < ActiveRecord::Base
+  validates_presence_of :text
+
   class << self
     def random
-      "some quip"
+      # see <http://almosteffortless.com/2007/12/04/random-records-in-rails/>
+      ids = connection.select_all("SELECT id FROM #{table_name}")
+      find(ids[rand(ids.length)]["id"].to_i).text unless ids.blank?
     end
   end
 end
