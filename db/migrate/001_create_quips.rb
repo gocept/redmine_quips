b4d4c00a13a7c671d368aab932c8abab9272d09d
Wolfgang Schnerring <ws@gocept.com>
2009-03-20 07:07:24 +0000

    add an example quip in the migration

--- db/migrate/001_create_quips.rb
+++ db/migrate/001_create_quips.rb
@@ -3,6 +3,9 @@ class CreateQuips < ActiveRecord::Migration
     create_table :quips do |t|
       t.column :text, :string
     end
+    q = Quip.new
+    q.text = 'Add some quips!'
+    q.save
   end
 
   def self.down
