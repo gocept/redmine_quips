b8ad7c503f64910bbdc284d2f3fd9f62ba37d9e4
Wolfgang Schnerring <ws@gocept.com>
2009-03-20 07:00:00 +0000

    inject quip display via javascript, so we don't need to copy any template code to override it

--- init.rb
+++ init.rb
@@ -1,8 +1,16 @@
 require 'redmine'
+require 'redmine/hook'
+
 
 Redmine::Plugin.register :redmine_quips do
   name 'Redmine Quips plugin'
   author 'Wolfgang Schnerring <ws@gocept.com>'
   description 'Display a random quip on the issue list'
   version '0.0.1'
+end
+
+
+class QuipHook < Redmine::Hook::ViewListener
+  render_on :view_layouts_base_content, :partial => "quips/inject"
+  render_on :view_layouts_base_html_head, :partial => "quips/head"
 end
